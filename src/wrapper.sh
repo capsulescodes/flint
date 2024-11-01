b87785adc715b5dcb9d540702f998d6887b438a7
mho22 <yannick@capsules.codes>
2024-11-01 16:01:48 +0100

    Refactored code and files

--- src/wrapper.sh
+++ src/wrapper.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
 
-if [[ -f "$PWD/$hooks/pre-$1" ]] && [ ! -f "$config" ]
+if [ ! -f "$config" ]
 
 then
     printf "\n\033[1;33mWarning : The \"$config\" file does not exist in the root directory. Running default git.\033[0m\n\n"
+else
+    source "$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )/functions.sh"
 fi
 
 
 
-if [[ -f "$PWD/$hooks/pre-$1" ]] && [ -f "$config" ] && [[ -f "$PWD/$hooks/pre-$1" ]]
+
+if [ -f "$config" ] && [[ -f "$PWD/$hooks/pre-$1" ]]
 
 then
     export FLINT_CONFIG="$config"
 
     export FLINT_HOOKS="$hooks"
 
+    export -f format_for_remote
+
     "$PWD/$hooks/pre-$1"
 
     unset FLINT_CONFIG
 
     unset FLINT_HOOKS
+
+    unset -f format_for_remote
 fi
 
 
@@ -33,18 +40,22 @@ return="$?"
 
 
 
-if [[ -f "$PWD/$hooks/post-$1" ]] && [ -f "$config" ] && [[ -f "$PWD/$hooks/post-$1" ]]
+if [ -f "$config" ] && [[ -f "$PWD/$hooks/post-$1" ]]
 
 then
     export FLINT_CONFIG="$config"
 
     export FLINT_HOOKS="$hooks"
 
+    export -f format_for_local
+
     "$PWD/$hooks/post-$1"
 
     unset FLINT_CONFIG
 
     unset FLINT_HOOKS
+
+    unset -f format_for_local
 fi
 
 
--- tests/helpers.sh
+++ tests/helpers.sh
@@ -5,25 +5,25 @@ assert()
     then
         echo "\n\033[1;31mAssertion failed : $1.\033[0m\n"
 
-        return 1
+        code=1
     fi
-
-    return 0
 }
 
 
 test()
 {
+    code=0
+
     $1
 
-    if [ "$?" -eq 0 ]
+    if [ "$code" -eq 0 ]
 
     then
         echo "\033[1;32m\xE2\x9C\x94 \033[1;30m$1\033[0m"
 
         return 0
     else
-            echo "\033[1;31m\xE2\x9C\x96 $1\033[0m"
+        echo "\033[1;31m\xE2\x9C\x96 $1\033[0m"
 
         return 1
     fi
