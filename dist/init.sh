4f5f508c9bc369f2bea4a6148131eedc26ac0e95
mho22 <yannick@capsules.codes>
2024-12-05 12:12:18 +0100

    Improved command line experience

--- dist/init.sh
+++ dist/init.sh
@@ -108,7 +108,7 @@ then
     if [ -f "$profile" ]
 
     then
-        command='git() { [[ -f "$PWD/.flint/git.sh" ]] && sh "$PWD/.flint/git.sh" || command git "$@" }'
+        command='git() { [[ -f "$PWD/.flint/git.sh" ]] && sh "$PWD/.flint/git.sh" "$@" || command git "$@" }'
 
         if [[ ! "$( cat $profile | tr -d '[:space:]' )" == *"$( echo $command | tr -d '[:space:]' )"* ]]
 
